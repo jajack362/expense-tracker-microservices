22004e41193b6ada3a061f29e6b15f5e42622c74
jajack362 <jaqthrowaway@gmail.com>
2025-06-22 23:05:05 +0100

    Start project for expense-service

---
+++ expense-service/src/main/java/com/expensetracker/expenseservice/util/UUIDv7Generator.java
@@ -0,0 +1,19 @@
+package com.expensetracker.expenseservice.util;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+import org.springframework.lang.NonNull;
+
+import java.io.Serializable;
+
+public class UUIDv7Generator implements IdentifierGenerator {
+
+  @Override
+  @NonNull
+  public Serializable generate(SharedSessionContractImplementor session, Object object)
+      throws HibernateException {
+    return UuidCreator.getTimeOrderedEpoch();
+  }
+}
